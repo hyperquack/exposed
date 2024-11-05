c272f61c84cd7c4de244a565dac1832351fb48ee
hyperquack <d@pinkduckcompany.com>
2024-11-05 13:43:28 -0500

    make optional param

--- main.go
+++ main.go
@@ -87,7 +87,7 @@ var readCmd = &cobra.Command{
 
 		if resp, err := client.Pull(namespace, host); err == nil {
 			for _, hit := range resp.Hits {
-				fmt.Println(hit.Value)
+				fmt.Println(hit.Host, hit.Value)
 			}
 		}
 	},
